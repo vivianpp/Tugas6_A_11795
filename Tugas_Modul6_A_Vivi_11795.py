d9763b31852b4d9933628a3a86b79f5c39345c76
vivianpp <220711795@students.uajy.ac.id>
2024-11-28 14:57:24 +0700

    Update Tugas_Modul6_A_Vivi_11795.py

--- Tugas_Modul6_A_Vivi_11795.py
+++ Tugas_Modul6_A_Vivi_11795.py
@@ -4,7 +4,7 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from PIL import Image
 
-model = load_model(r"C:\Users\ASUS\Documents\PMDPM\Tugas6_A_11795\model_mobilenet.h5")
+model = load_model(r'model_mobilenet.h5')
 class_names = ['Matang', 'Mentah']
 
 def classify_image(image_path):
